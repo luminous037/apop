648be24f80edbe79f37d030f0a63df8f3f542594
bitesnail <rkwoal216@gmail.com>
2023-11-17 18:45:11 +0900

    feat: Question 모델에 질문 타입 추가

--- survey/models/question.py
+++ survey/models/question.py
@@ -1,7 +1,21 @@
+from collections import namedtuple
+
 from django.db import models
+
+TYPE_FILED = namedtuple(
+    "TYPE_FIELD", "text number radio select multi_select text_area"
+)._make(range(6))
 
 
 class Question(models.Model):
+    TYPE_FILED = [
+        (TYPE_FILED.text, "text"),
+        (TYPE_FILED.number, "number"),
+        (TYPE_FILED.radio, "radio"),
+        (TYPE_FILED.select, "select"),
+        (TYPE_FILED.multi_select, "multi_select"),
+        (TYPE_FILED.text_area, "text_area"),
+    ]
     title = models.CharField(
         db_column="title",
         db_comment="question title",
@@ -33,6 +47,12 @@ class Question(models.Model):
     answers = models.ManyToManyField(
         to="Answer",
         through="QuestionAnswer",
+    )
+    type_filed = models.PositiveSmallIntegerField(
+        db_column="type_filed",
+        db_comment="question type",
+        choices=TYPE_FILED,
+        help_text="질문 타입"
     )
 
     class Meta:
