ab90cfc4ee6fab3be82dc0f4f0458f4ecb026dc0
bitesnail <rkwoal216@gmail.com>
2023-12-18 11:25:18 +0900

    fix: UserSurvey 검색 되지 않았던 문제 해결

--- survey/admins/user_survey_admin.py
+++ survey/admins/user_survey_admin.py
@@ -11,7 +11,7 @@ class UserSurveyAdmin(admin.ModelAdmin):
     fields = ['user', 'survey']
     list_display = ['user', 'survey', 'create_at']
     ordering = ['user', 'create_at']
-    search_fields = ['user', 'survey']
+    search_fields = ['user__username', 'survey__title']
     search_help_text = "유저나 설문조사를 검색할 수 있습니다."
     
     inlines = [ReplyInline]
