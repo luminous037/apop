94d1fc280bb88944d9c1af5ebd6a3262a8d3d1d2
bitesnail <rkwoal216@gmail.com>
2023-12-18 11:13:54 +0900

    feat: 설문 작성 완료 구현

--- survey/views.py
+++ survey/views.py
@@ -1,6 +1,6 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from survey.models import Survey
+from survey.models import Survey, Question, UserSurvey, Reply, SurveyQuestion
 from django.views.generic.edit import ProcessFormView
 from django.shortcuts import render
 
@@ -23,7 +23,28 @@ class SurveyFormView(ProcessFormView):
         return render(request, 'survey/user_survey_form.html', context)
     
     def post(self, request, *args, **kwargs):
-        print(request.POST.keys())
+        survey = Survey.objects.get(**kwargs)
+        user_survey = UserSurvey.objects.get_or_create(user=request.user,
+                                                       survey=survey)
+        
+        if user_survey[1] == False:
+            """
+            error 발생
+            """
+            pass
+        
+        user_survey=user_survey[0]
+        
+        for key, value in request.POST.items():
+            if not key.isdigit():
+                continue
+            question = Question.objects.get(pk=int(key))
+            
+            Reply.objects.create(user_survey=user_survey,
+                                 survey_question=SurveyQuestion.objects.get(survey=survey,
+                                                                            question=question),
+                                 content=value)
+        
         return render(request, 'survey/survey_complete.html')
     
     def put(self, *args, **kwargs):
