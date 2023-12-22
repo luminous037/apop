65a09a39dbf3582008cc6e8144e1a3690f04f075
bitesnail <rkwoal216@gmail.com>
2023-12-22 19:08:15 +0900

    feat: logout 기능 추가

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
-from django.contrib.auth.views import LoginView as Login
+from django.contrib.auth.views import LoginView as Login, LogoutView as Logout
 from .forms import MyAuthenticationForm
 
 # Create your views here.
 class LoginView(Login):
     template_name = 'accounts/login.html'
     redirect_field_name = 'redirect_to'
-    # form_class = MyAuthenticationForm
+    next_page= '/'
+
+    
+class LogoutView(Logout):
+    template_name = 'accounts/login.html'
+    redirect_field_name = 'redirect_to'
+    next_page = '/'
+    
