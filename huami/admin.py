133c5149de20572b501888039aa3688c0fc7941a
BiteSnail <rkwoal216@gmail.com>
2024-01-03 15:21:57 +0900

    feat: 어드민 페이지에서 참여자 데이터 수집 여부 확인 기능 추가

--- huami/admin.py
+++ huami/admin.py
@@ -16,9 +16,28 @@ class HuamiAccountAdmin(admin.ModelAdmin):
 class HealthDataAdmin(admin.ModelAdmin):
     """HealthData 모델 어드민
     """    
+    list_display = ['full_name', 'date', 'heart', 'sleeps', 'steps', 'stresses', 'blood']
     ordering = ['huami_account', 'date']
     search_fields = ['huami_account__user__username', 'date']
     search_help_text = "유저나 날짜로 검색할 수 있습니다."
+    
+    def full_name(self, obj: HealthData):
+        return obj.huami_account.full_name
+
+    def heart(self, obj: HealthData):
+        return obj.heart_rate is not None
+    
+    def sleeps(self, obj: HealthData):
+        return obj.sleep_quality is not None
+    
+    def steps(self, obj: HealthData):
+        return obj.step_count is not None
+    
+    def stresses(self, obj: HealthData):
+        return obj.stress is not None
+    
+    def blood(slef, obj: HealthData):
+        return obj.spo2 is not None
 
 admin.site.register(HuamiAccount, HuamiAccountAdmin)
 admin.site.register(HealthData, HealthDataAdmin)
