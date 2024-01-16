7eadda5258d51f6568d57030ba444a4662c68344
BiteSnail <rkwoal216@gmail.com>
2024-01-16 21:21:01 +0900

    feat: db compose 수정

--- apop2/settings.py
+++ apop2/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-atqfh&#+x!)pdp=vk(3fzo))nv9%u811p18t2_l3ta9=eqw1ln'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -82,16 +82,12 @@ WSGI_APPLICATION = 'apop2.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        "NAME": "apop",
+        "NAME": "apopdb",
         "USER": "example-user", 
         "PASSWORD": "example-password", 
-        # "HOST": ,
+        "HOST": "mariadb",
         "PORT": "3306"
     },
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
 }
 
 # Password validation
